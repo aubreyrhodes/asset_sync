4a94027931009bcad3c8f5de62a9c145c00ab10d
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2013-01-13 22:34:08 -0500

    update cache-control to one hour

--- lib/asset_sync/storage.rb
+++ lib/asset_sync/storage.rb
@@ -104,13 +104,13 @@ module AssetSync
 
     def upload_file(f)
       # TODO output files in debug logs as asset filename only.
-      one_year = 31557600
+      one_hour = 3600
       file = {
         :key => f,
         :body => File.open("#{path}/#{f}"),
         :public => true,
-        :cache_control => "public, max-age=#{one_year}",
-        :expires => CGI.rfc1123_date(Time.now + one_year)
+        :cache_control => "public, max-age=#{one_hour}",
+        :expires => CGI.rfc1123_date(Time.now + one_hour)
       }
 
       gzipped = "#{path}/#{f}.gz"
